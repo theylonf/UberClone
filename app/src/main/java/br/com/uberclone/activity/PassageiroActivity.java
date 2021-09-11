8353005d0b969ada4f1e4a03e8e5d7c726faa995
Onkos123 <theylonfeitosa@gmail.com>
2021-09-11 17:43:17 -0300

    retrieving destination address

--- app/src/main/java/br/com/uberclone/activity/PassageiroActivity.java
+++ app/src/main/java/br/com/uberclone/activity/PassageiroActivity.java
@@ -2,13 +2,19 @@ package br.com.uberclone.activity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,19 +26,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.core.app.ActivityCompat;
 import androidx.navigation.ui.AppBarConfiguration;
 
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import br.com.uberclone.R;
 import br.com.uberclone.config.ConfiguracaoFirebase;
 import br.com.uberclone.databinding.ActivityPassageiroBinding;
+import br.com.uberclone.model.Destino;
 
 public class PassageiroActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private AppBarConfiguration appBarConfiguration;
     private GoogleMap mMap;
     private ActivityPassageiroBinding binding;
     private FirebaseAuth auth;
@@ -43,21 +54,11 @@ public class PassageiroActivity extends AppCompatActivity implements OnMapReadyC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityPassageiroBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-        binding.toolbar.setTitle(getString(R.string.startRun));
-
-        //Configurações iniciais
-        auth = ConfiguracaoFirebase.getFirebaseAuth();
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        inicializarComponentes();
 
     }
+
+
 
     /**
      * Manipulates the map once available.
@@ -73,6 +74,68 @@ public class PassageiroActivity extends AppCompatActivity implements OnMapReadyC
         mMap = googleMap;
 
         recuperarLocalizacaoUsuario();
+    }
+
+    public void chamarUber(View view) {
+        String textoDestino = binding.editDestino.getText().toString();
+
+        if (!textoDestino.equals("") || textoDestino != null){
+            Address addressDestino = recuperarEndereco(textoDestino);
+            if (addressDestino != null){
+                Destino destino = new Destino();
+                destino.setCidade(addressDestino.getAdminArea());
+                destino.setCep(addressDestino.getPostalCode());
+                destino.setBairro(addressDestino.getSubLocality());
+                destino.setRua(addressDestino.getThoroughfare());
+                destino.setNumero(addressDestino.getFeatureName());
+
+                destino.setLatitude(String.valueOf(addressDestino.getLatitude()));
+                destino.setLongitude(String.valueOf(addressDestino.getLongitude()));
+
+                StringBuilder menssagem = new StringBuilder();
+                menssagem.append(getString(R.string.cityDestiny)+ destino.getCidade());
+                menssagem.append(getString(R.string.ThoroughfareDestiny)+ destino.getRua());
+                menssagem.append(getString(R.string.subLocalityDestiny)+destino.getBairro());
+                menssagem.append(getString(R.string.NumberLocality)+destino.getNumero());
+                menssagem.append(getString(R.string.postalCodeDestiny)+destino.getNumero());
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.confirmAddrs))
+                        .setMessage(menssagem)
+                        .setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Salvar requisição
+                            }
+                        }).setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
+            }
+        }else {
+            Toast.makeText(this, R.string.setDestinoAddrs, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Address recuperarEndereco(String endereco){
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> listaEnderecos = geocoder.getFromLocationName(endereco,1);
+            if (listaEnderecos != null && listaEnderecos.size() > 0){
+                Address address = listaEnderecos.get(0);
+
+                return address;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void recuperarLocalizacaoUsuario() {
@@ -144,5 +207,24 @@ public class PassageiroActivity extends AppCompatActivity implements OnMapReadyC
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void inicializarComponentes() {
+
+        binding = ActivityPassageiroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setTitle(getString(R.string.startRun));
+
+        //Configurações iniciais
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
     }
 }
---
+++ app/src/main/java/br/com/uberclone/model/Destino.java
@@ -0,0 +1,70 @@
+package br.com.uberclone.model;
+
+public class Destino {
+    private String rua
+            , numero
+            , cidade
+            , bairro
+            , cep
+            , latitude
+            , longitude;
+
+    public Destino() {
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
